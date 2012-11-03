294c904372b198e59b406c7f0e40cc8a25399ce4
lmbacelar <lmbacelar@gmail.com>
2012-11-03 01:21:05 +0000

    variable renaming

--- app/services/rtd.rb
+++ app/services/rtd.rb
@@ -1,10 +1,10 @@
 class RTD
   def self.temperature(params)
-    type = params.delete(:type) { |el| raise "#{el} is required!" }
+    type = params.delete(:type) { |t| raise "#{t} is required!" }
     case type
     when :iec60751 then IEC60751.temperature(params)
+    when :sprt     then SPRT.temperature(params)
     else raise "type '#{type}' not expected"
     end
-    
   end
 end
