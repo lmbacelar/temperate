02f0b5f5d359cf858d1b73abba27f538934cfb0b
lmbacelar <lmbacelar@gmail.com>
2012-11-03 06:48:33 +0000

    implemented SPRT ITS'90 reference functions wr and t90

--- app/services/sprt.rb
+++ app/services/sprt.rb
@@ -1,21 +1,73 @@
 class SPRT
 
-  def self.wref(params)
+  MAX_ITERATIONS = 50
+  LLIMIT = -259.4467
+  ULIMIT = 961.88
+                  
+  def self.wr(params)
     parse params
-    raise 'temperature is required!' unless @@temperature
-    raise 'out of bounds!' if !@@skip_check && (@@temperature < -200.00 || @@temperature > 850.00)
+    raise 't90 is required!' unless @@t90
+    wr_celsius
+  end
 
+  def self.t90(params)
+    parse params
+    raise 'wr is required!' unless @@wr
+    Unit.convert(t: t90_celsius, from: :celsius, to: @@unit)
   end
 
 private
-  def self.parse(params)
-    @@temperature = params[:temperature]
-    @@unit = params[:unit] || :celsius
-    ###@@max_error   = params[:max_error]   ||  0.0001
-    ###@@skip_check  = params[:skip_check]  ||  false
-    ###@@r0          = params[:R0]          ||  100.0
-    ###@@a           = params[:A]           ||  3.9083e-03
-    ###@@b           = params[:B]           || -5.7750e-07
-    ###@@c           = params[:C]           || -4.1830e-12
+  def self.parse(params={})
+    @@r           = params[:r]
+    @@rtpw        = params[:rtpw]
+    @@t90         = params[:t90]
+    @@wr          = params[:wr]
+    @@unit        = params[:unit] ||  :celsius
+
+    @@t90c        = Unit.convert(t: @@t90, from: @@unit, to: :celsius) if @@t90
+  end
+
+  def self.wr_celsius
+    raise 'out of bounds!' if @@t90c < LLIMIT || @@t90c > ULIMIT
+    if @@t90c < 0.01
+      a = [ -2.13534729, 3.1832472,  -1.80143597,
+             0.71727204, 0.50344027, -0.61899395,
+            -0.05332322, 0.28021362,  0.10715224,
+            -0.29302865, 0.04459872,  0.11868632,
+            -0.05248134 ]
+      Math.exp(a.each_with_index.map{ |a, i| a*( (Math.log((@@t90c+273.15)/273.16) + 1.5)/1.5 )**i }.inject(:+))
+    else
+      c = [  2.78157254,  1.64650916, -0.1371439,
+            -0.00649767, -0.00234444,  0.00511868,
+             0.00187982, -0.00204472, -0.00046122,
+             0.00045724 ]
+      c.each_with_index.map{ |c, i| c*((@@t90c-481)/481)**i }.inject(:+)
+    end
+  end
+
+  def self.t90_celsius
+    if @@wr < 1.0
+      b = [  0.183324722,  0.240975303,  0.209108771,
+             0.190439972,  0.142648498,  0.077993465,
+             0.012475611, -0.032267127, -0.075291522,
+            -0.05647067,   0.076201285,  0.123893204,
+            -0.029201193, -0.091173542,  0.001317696,
+             0.026025526 ]
+      t = b.each_with_index.map{ |b, i| b*(((@@wr)**(1.0/6)-0.65)/0.35)**i }.inject(:+) * 273.16 - 273.15
+    else
+      d = [ 439.932854,
+            472.41802,
+             37.684494,
+              7.472018,
+              2.920828,
+              0.005184,
+             -0.963864,
+             -0.188732,
+              0.191203,
+              0.049025 ]
+      t = d.each_with_index.map{ |d, i| d*((@@wr-2.64)/1.64)**i }.inject(:+)
+    end
+    raise 'out of bounds!' if t < LLIMIT || t > ULIMIT
+    t
   end
 end
--- spec/services/sprt_spec.rb
+++ spec/services/sprt_spec.rb
@@ -1,26 +1,63 @@
 require_relative '../../app/services/sprt'
+require_relative '../../app/services/unit'
 
 describe SPRT do
-  context 'WRef conputation' do
-    it 'handles negative temperatures' do
-      #SPRT.wref(temperature:  13.8033, unit: :kelvin).should be_within(0.0001).of(-200.0000)
-      #SPRT.wref(temperature:  83.8058, unit: :kelvin).should be_within(0.0001).of(-100.0000)
-      SPRT.wref(temperature: 273.16  , unit: :kelvin).should be_within(0.0001).of(0.0000)
+  context 'reference funtions' do
+    examples = [ { point: 'e-H2 Tp', t90: -259.3467, wr: 0.00119007 }, 
+                 { point: 'Ne Tp',   t90: -248.5939, wr: 0.00844974 }, 
+                 { point: 'O2 Tp',   t90: -218.7916, wr: 0.09171804 }, 
+                 { point: 'Ar Tp',   t90: -189.3442, wr: 0.21585975 }, 
+                 { point: 'Hg Tp',   t90:  -38.8344, wr: 0.84414211 }, 
+                 { point: 'H2O Tp',  t90:    0.01,   wr: 1.00000000 }, 
+                 { point: 'Ga Mp',   t90:   29.7646, wr: 1.11813889 }, 
+                 { point: 'In Fp',   t90:  156.5985, wr: 1.60980185 }, 
+                 { point: 'Sn Fp',   t90:  231.9280, wr: 1.89279768 }, 
+                 { point: 'Zn Fp',   t90:  419.5270, wr: 2.56891730 }, 
+                 { point: 'Al Fp',   t90:  660.3230, wr: 3.37600860 }, 
+                 { point: 'Ag Fp',   t90:  961.7800, wr: 4.28642053 } ] 
+
+    context 'wr computation' do
+      examples.each do |ex|
+        it "handles #{ex[:point]} fixed point" do
+          SPRT.wr(t90: ex[:t90]).should be_within(0.0000001).of(ex[:wr])
+        end
+      end
+
+      it 'handles kelvin units' do
+        SPRT.wr(t90: 273.16, unit: :kelvin).should be_within(0.0000001).of(1.00000000)
+      end
+
+      it 'raises error when out of bounds' do
+        expect { SPRT.wr(t90: -259.50) }.to raise_error
+        expect { SPRT.wr(t90:  961.90) }.to raise_error
+      end
+
+      it 'raises error when no t90 is set' do
+        expect { SPRT.wr }.to raise_error
+        expect { SPRT.wr(dummy: :dummy) }.to raise_error
+      end
     end
 
-    xit 'handles positive temperatures' do
-      SPRT.wref(temperature:  273.16 , unit: :kelvin).should be_within(0.0001).of(0.0000)
-      SPRT.wref(temperature:  933.473, unit: :kelvin).should be_within(0.0001).of(400.0000)
-      SPRT.wref(temperature: 1234.93 , unit: :kelvin).should be_within(0.0001).of(850.0000)
-    end
+    context 't90 computation' do
+      examples.each do |ex|
+        it "handles #{ex[:point]} fixed point" do
+          SPRT.t90(wr: ex[:wr]).should be_within(0.00013).of(ex[:t90])
+        end
+      end
 
-    xit 'handls degrees Celsius' do
-      SPRT.wref(temperature: 0.01  , unit: :celsius).should be_within(0.0001).of(0.0000)
-    end
+      it 'handles kelvin units' do
+        SPRT.t90(wr: 1.00000000, unit: :kelvin).should be_within(0.00013).of(273.16)
+      end
 
-    xit 'raises error when out of bounds' do
-      expect { SPRT.wref(temperature:   13.79, unit: :kelvin) }.to raise_error
-      expect { SPRT.wref(temperature: 1235.00, unit: :kelvin) }.to raise_error
+      it 'raises error when out of bounds' do
+        expect { SPRT.t90(wr: 0.001) }.to raise_error
+        expect { SPRT.t90(wr: 4.290) }.to raise_error
+      end
+
+      it 'raises error when no wr is set' do
+        expect { SPRT.t90 }.to raise_error
+        expect { SPRT.t90(dummy: :dummy) }.to raise_error
+      end
     end
   end
 end
