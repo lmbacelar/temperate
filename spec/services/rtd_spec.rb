d555ac9a4c3099c671c8f32e7122fc5f9a90003d
lmbacelar <lmbacelar@gmail.com>
2012-11-03 04:02:22 +0000

    added sprt spec

--- spec/services/rtd_spec.rb
+++ spec/services/rtd_spec.rb
@@ -1,10 +1,16 @@
 require_relative '../../app/services/rtd'
 require_relative '../../app/services/iec60751'
+require_relative '../../app/services/sprt'
 
 describe RTD do
   it 'handles IEC 60751' do
     IEC60751.should_receive(:temperature).with(resistance: 100.0)
     RTD.temperature(type: :iec60751, resistance: 100.0)
+  end
+
+  it 'handles SPRT' do
+    SPRT.should_receive(:temperature).with(resistance: 25.0)
+    RTD.temperature(type: :sprt, resistance: 25.0)
   end
 
   it 'raises exception when no type is set' do
