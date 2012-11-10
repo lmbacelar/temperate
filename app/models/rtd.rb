56e02142761a94ae42a15c3fb3e952b81ee5e1f8
lmbacelar <lmbacelar@gmail.com>
2012-11-10 01:45:53 +0000

    add rtd model specs and behaviour

--- app/models/rtd.rb
+++ app/models/rtd.rb
@@ -1,3 +1,8 @@
 class Rtd < ActiveRecord::Base
-  attr_accessible :a, :b, :c, :decimal, :decimal, :name, :r0
+  attr_accessible  :name, :r0, :a, :b, :c
+  
+  validates_uniqueness_of   :name
+  validates_presence_of     :name, :r0, :a, :b, :c
+  validates_numericality_of :r0, :a, :b, :c
+
 end
--- spec/models/rtd_spec.rb
+++ spec/models/rtd_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Rtd do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let (:rtd) { Rtd.new(name: 'rtd1') }
+
+  it "remembers rtd's" do
+    rtd.save
+    expect { Rtd.find_by_name!('rtd1') }.to_not raise_error ActiveRecord::RecordInvalid  
+  end
+
+  it 'should have a unique name' do
+    rtd.save
+    expect { Rtd.create!(name: rtd.name) }.to raise_error ActiveRecord::RecordInvalid
+  end
+
+  [:r0, :a, :b, :c].each do |k|
+    it "should have a numeric, non empty #{k}" do
+      rtd.update_attributes(k => nil)
+      expect { rtd.save! }.to raise_error ActiveRecord::RecordInvalid
+      rtd.update_attributes(k => 'a')
+      expect { rtd.save! }.to raise_error ActiveRecord::RecordInvalid
+    end
+  end
+
+  it 'should have default constants according to IEC60751' do
+    rtd.r0.should == 100.0
+    rtd.a.should  ==   3.9083e-03
+    rtd.b.should  ==  -5.7750e-07
+    rtd.c.should  ==  -4.1830e-12
+  end
 end
