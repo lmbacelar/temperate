97ffd0239954c454d76ff083e9978d61eab77860
lmbacelar <lmbacelar@gmail.com>
2012-11-10 01:46:04 +0000

    add sprt model specs and behaviour

--- app/models/sprt.rb
+++ app/models/sprt.rb
@@ -1,3 +1,7 @@
 class Sprt < ActiveRecord::Base
-  attr_accessible :a, :b, :c, :c1, :c2, :c3, :c4, :c5, :d, :name, :range, :rtpw, :w660
+  attr_accessible :name, :range, :rtpw, :a, :b, :c, :d, :w660, :c1, :c2, :c3, :c4, :c5
+
+  validates_uniqueness_of   :name
+  validates_presence_of     :name, :range, :rtpw, :a, :b, :c, :d, :w660, :c1, :c2, :c3, :c4, :c5
+  validates_numericality_of :range, :rtpw, :a, :b, :c, :d, :w660, :c1, :c2, :c3, :c4, :c5
 end
--- spec/models/sprt_spec.rb
+++ spec/models/sprt_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Sprt do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let (:sprt) { Sprt.new(name: 'sprt1') }
+
+  it "remembers sprt's" do
+    sprt.save
+    expect { Sprt.find_by_name!('sprt1') }.to_not raise_error ActiveRecord::RecordInvalid  
+  end
+
+  it 'should have a unique name non empty' do
+    sprt.save
+    expect { Sprt.create!(name: sprt.name) }.to raise_error ActiveRecord::RecordInvalid
+    expect { Sprt.create!(name: nil)       }.to raise_error ActiveRecord::RecordInvalid
+    expect { Sprt.create!(name: '')        }.to raise_error ActiveRecord::RecordInvalid
+  end
+
+  [:range, :rtpw, :a, :b, :c, :d, :w660, :c1, :c2, :c3, :c4, :c5].each do |k|
+    it "should have a numeric, non empty #{k}" do
+      sprt.update_attributes(k => nil)
+      expect { sprt.save! }.to raise_error ActiveRecord::RecordInvalid
+      sprt.update_attributes(k => 'a')
+      expect { sprt.save! }.to raise_error ActiveRecord::RecordInvalid
+    end
+  end
+
+  it 'should have default constants according to ITS-90' do
+    sprt.range.should == 11
+    sprt.rtpw.should  == 25.0
+    [:a, :b, :c, :d, :w660, :c1, :c2, :c3, :c4, :c5].each do |k|
+      sprt.send(k).should == 0.0
+    end
+  end
 end
