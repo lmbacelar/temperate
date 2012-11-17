41c6020629f54b1af15baba2ef99ad5f07bbe9e7
lmbacelar <lmbacelar@gmail.com>
2012-11-17 04:06:37 +0000

    add temperature functions for all tc types except type c. Add range check. Bug exists with test data setup on tests. Serialize examples?

--- spec/services/iec60584_spec.rb
+++ spec/services/iec60584_spec.rb
@@ -1,8 +1,7 @@
 require_relative '../../app/services/iec60584'
 
 describe Iec60584 do
-  examples = { b: [ { e:  0.000, t:     0.0 },
-                    { e:  0.291, t:   250.0 },
+  examples = { b: [ { e:  0.291, t:   250.0 },
                     { e:  1.975, t:   630.0 },
                     { e:  1.981, t:   631.0 },
                     { e:  5.780, t:  1100.0 },
@@ -13,7 +12,7 @@ describe Iec60584 do
                     { e: 20.066, t:  1100.0 },
                     { e: 29.402, t:  1680.0 },
                     { e: 37.107, t:  2320.0 } ], 
-               e: [ { e: -9.835, t:  -270.0 },
+               e: [ { e: -8.825, t:  -200.0 },
                     { e: -5.237, t:  -100.0 },
                     { e:  0.000, t:     0.0 },
                     { e:  6.319, t:   100.0 },
@@ -25,13 +24,13 @@ describe Iec60584 do
                     { e: 36.675, t:   660.0 }, 
                     { e: 43.559, t:   770.0 }, 
                     { e: 69.553, t:  1200.0 } ],
-               k: [ { e: -6.458, t:  -270.0 }, 
+               k: [ { e: -5.891, t:  -200.0 }, 
                     { e: -3.554, t:  -100.0 }, 
                     { e:  0.000, t:     0.0 }, 
                     { e:  5.206, t:   127.0 }, 
                     { e: 27.447, t:   660.0 }, 
                     { e: 54.886, t:  1372.0 } ],
-               n: [ { e: -4.345, t:  -270.0 },
+               n: [ { e: -3.990, t:  -200.0 },
                     { e: -2.407, t:  -100.0 },
                     { e:  0.000, t:     0.0 },
                     { e:  2.774, t:   100.0 },
@@ -49,7 +48,7 @@ describe Iec60584 do
                     { e:  5.857, t:   660.0 },
                     { e: 10.757, t:  1100.0 },
                     { e: 18.693, t:  1768.0 } ],
-               t: [ { e: -6.258, t:  -270.0 },
+               t: [ { e: -5.603, t:  -200.0 },
                     { e: -3.379, t:  -100.0 },
                     { e:  0.000, t:     0.0 },
                     { e:  4.279, t:   100.0 },
@@ -57,7 +56,7 @@ describe Iec60584 do
                     { e: 20.872, t:   400.0 } ]
              }
   context 'temperature computation' do
-    [:b].each do |kind|
+    [:b, :e, :j, :k, :n, :r, :s, :t].each do |kind|
       context "on a type #{kind} thermocouple" do
         let(:tc) { stub(kind: kind, a3: 0.0, a2: 0.0, a1: 0.0, a0: 0.0) }
         examples[kind].each do |ex|
@@ -66,15 +65,21 @@ describe Iec60584 do
           end
         end
 
-        #it 'raises error when out of range' do
-        #  expect { Iec60584.temperature(tc,  18.00) }.to raise_error(RangeError)
-        #  expect { Iec60584.temperature(tc, 391.00) }.to raise_error(RangeError)
-        #end
+        it 'raises error when out of range' do
+          expect { Iec60584.temperature(tc, examples[kind].first[:e] - 0.002) }.to raise_error(RangeError)
+          expect { Iec60584.temperature(tc, examples[kind].last[:e]  + 0.002) }.to raise_error(RangeError)
+        end
       end
     end
   end
 
   context 'emf computation' do
+    # Add lower limit form emf equations, if they are lower than the lower limits for temperature equations
+    examples[:b].unshift({ e:  0.000, t:     0.0 })
+    examples[:e].unshift({ e: -9.835, t:  -270.0 })
+    examples[:k].unshift({ e: -6.458, t:  -270.0 }) 
+    examples[:n].unshift({ e: -4.345, t:  -270.0 })
+    examples[:t].unshift({ e: -6.258, t:  -270.0 })
     [:b, :c, :e, :j, :k, :n, :r, :s, :t].each do |kind|
       context "on a type #{kind} thermocouple" do
         let(:tc) { stub(kind: kind, a3: 0.0, a2: 0.0, a1: 0.0, a0: 0.0) }
